ae908424e96cb8a3b2a28362fc0e4a626b83e2cb
fumyparli <qkskskn@naver.com>
2021-04-14 17:30:30 +0900

    Make basic structure

---
+++ src/main/java/ga/fumyparli/messagingstomepwebsocket/Greeting.java
@@ -0,0 +1,20 @@
+package ga.fumyparli.messagingstomepwebsocket;
+
+public class Greeting {
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
